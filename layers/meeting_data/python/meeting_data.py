62ccd7d49bd866d6037040be49209bac622da0c1
nriley-14 <njriley@calpoly.edu>
2025-10-23 09:25:59 -0700

    Add lambda data layer

---
+++ layers/meeting_data/python/meeting_data.py
@@ -0,0 +1,17 @@
+"""
+Meeting data manager module for Lambda layer
+"""
+
+class MeetingDataManager:
+    """Placeholder class for meeting data management"""
+    
+    def __init__(self, table_name):
+        self.table_name = table_name
+    
+    def get_project_data(self, project_id):
+        """Get project data"""
+        return {}
+    
+    def save_project_data(self, project_id, data):
+        """Save project data"""
+        pass
